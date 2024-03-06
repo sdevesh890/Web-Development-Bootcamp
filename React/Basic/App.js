59b01fa5bffa136f101dc0028369dce633fdb3ab
sdevesh890 <74634378+sdevesh890@users.noreply.github.com>
2024-03-06 07:20:10 +0530

    Basic set up of React

---
+++ React/Basic/App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement("h1",{
+    id:"heading",
+    class: "reactHead" ,
+    style : {
+        border: "2px solid black" , 
+        backgroundColor : "red",
+    } 
+},"My name is Devesh Shamra");
+
+const heading2 = React.createElement("h2",{
+
+    id : "heading2",
+    class : "reactHead2",
+    style: {
+        border : "2px solid black",
+        backgroundColor : "yellow"
+    }
+},"Here I'm learning React");
+
+const container = React.createElement("div",{},[heading,heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
